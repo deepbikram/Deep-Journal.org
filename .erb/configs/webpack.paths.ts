8d77e2eeb6245d630112d2151854a287735c53ff
Deep Bikram Thapa Chhetri <deepbikram01@gmail.com>
2025-06-19 19:45:19 +0545

    updates for the releases

--- .erb/configs/webpack.paths.ts
+++ .erb/configs/webpack.paths.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Use import.meta.url to get __dirname equivalent
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
